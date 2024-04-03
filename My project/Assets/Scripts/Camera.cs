499f2f8707bfe9ab2c9d730b16fc5bae57913989
Mateus Silva <132206573+mateussilvacustodio@users.noreply.github.com>
2024-04-03 11:29:28 -0300

    Segunda atividade

---
+++ My project/Assets/Scripts/Camera.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    
+    [SerializeField] Transform characterTransform;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (characterTransform.position.x > 11) {
+
+            if(characterTransform.position.y > 5) {
+
+                transform.position = new Vector3(11f, 5, -10);
+
+            } else if (characterTransform.position.y < -8) {
+
+                transform.position = new Vector3(11f, -8, -10);
+
+            } else {
+
+                transform.position = new Vector3(11f, characterTransform.position.y, -10);
+
+            }
+
+        } else if (characterTransform.position.x < -3) {
+
+            if(characterTransform.position.y > 5) {
+
+                transform.position = new Vector3(-3f, 5, -10);
+
+            } else if (characterTransform.position.y < -8) {
+
+                transform.position = new Vector3(-3f, -8, -10);
+
+            } else {
+
+                transform.position = new Vector3(-3f, characterTransform.position.y, -10);
+
+            }
+
+        } else {
+
+            if(characterTransform.position.y > 5) {
+
+                transform.position = new Vector3(characterTransform.position.x, 5, -10);
+
+            } else if (characterTransform.position.y < -8) {
+
+                transform.position = new Vector3(characterTransform.position.x, -8, -10);
+
+            } else {
+
+                transform.position = new Vector3(characterTransform.position.x, characterTransform.position.y, -10);
+
+            }
+            
+            
+
+        }        
+
+    }
+}
