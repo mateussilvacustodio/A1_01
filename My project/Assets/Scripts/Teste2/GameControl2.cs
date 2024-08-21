7ad466b6cc40cdd742488c75c8d5c02530013600
Mateus Silva <132206573+mateussilvacustodio@users.noreply.github.com>
2024-08-21 18:08:05 -0300

    Configurando rotacao do ponteiro, lerp do dinheiro e base do sistema de dias

--- My project/Assets/Scripts/Teste2/GameControl2.cs
+++ My project/Assets/Scripts/Teste2/GameControl2.cs
@@ -8,66 +8,94 @@ public class GameControl2 : MonoBehaviour
     [Header("Recursos")]
     public float dinheiro;
     [SerializeField] RectTransform ponteiroTransform;
-    public float anguloPonteiro;
+    Quaternion ponteiroRotacaoAlvo;
+    [SerializeField] float velocidadeRotacao;
+    [SerializeField] bool ponteiroEstaRodando;
+    public float quantidadePedidos;
+    public float dia;
     [Header("Personagens")]
-    [SerializeField] GameObject[] personagens;
-    [SerializeField] int personagemIndex;
+    public GameObject[] personagens;
+    public int personagemIndex;
     [Header("Balao")]
-    [SerializeField] Text balaoTexto;
+    public Text balaoTexto;
     [SerializeField] Text dinheiroTexto;
+    [SerializeField] Text diaTexto;
+    public Text nomeBalao;
     [Header("Botoes")]
-    [SerializeField] Text botaoConfirmacaoTexto;
-    [SerializeField] Button botaoConfirmacao;
-    [SerializeField] Text botaoNegacaoTexto;
-    [SerializeField] Button botaoNegacao;
+    public Text botaoConfirmacaoTexto;
+    public Button botaoConfirmacao;
+    public Text botaoNegacaoTexto;
+    public Button botaoNegacao;
     [Header("Lerp")]
     [SerializeField] float lerp;
     [SerializeField] float duracaoLerp;
+    [Header("Controle Aleatoriedade")]
+    public float controleAleatoriedade;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
+        
+        nomeBalao.text = personagens[personagemIndex].name;
         balaoTexto.text = personagens[personagemIndex].GetComponent<Perso2>().pedido;
         botaoConfirmacaoTexto.text = personagens[personagemIndex].GetComponent<Perso2>().opcao1;
         botaoNegacaoTexto.text = personagens[personagemIndex].GetComponent<Perso2>().opcao2;
 
+        ponteiroRotacaoAlvo = ponteiroTransform.rotation;
         botaoConfirmacao.onClick.AddListener(personagens[personagemIndex].GetComponent<Perso2>().Concordo);
         botaoNegacao.onClick.AddListener(personagens[personagemIndex].GetComponent<Perso2>().Discordo);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        ponteiroTransform.rotation = Quaternion.Euler(0,0,anguloPonteiro);
-        dinheiroTexto.text = dinheiro.ToString();
 
-        if(Input.GetKey(KeyCode.A)) {
+        diaTexto.text = "Dia " + dia.ToString();
+        dinheiroTexto.text = dinheiro.ToString("F0");
 
-            anguloPonteiro = anguloPonteiro + Time.deltaTime * 10;
+        if(ponteiroEstaRodando) {
+
+            ponteiroTransform.rotation = Quaternion.Lerp(ponteiroTransform.rotation, ponteiroRotacaoAlvo, Time.deltaTime * velocidadeRotacao);
+
+            if(Quaternion.Angle(ponteiroTransform.rotation, ponteiroRotacaoAlvo) < 0.1f) {
+
+                ponteiroTransform.rotation = ponteiroRotacaoAlvo;
+                ponteiroEstaRodando = false;
+
+            }
 
         }
+
     }
 
-    IEnumerator lerpReputacao(float mudador) {
+    public void comecarRodar(float quantidadeRotacaoP) {
+
+        ponteiroRotacaoAlvo *= Quaternion.Euler(0,0,quantidadeRotacaoP);
+        ponteiroEstaRodando = true;
+
+    }
+
+    IEnumerator lerpDinheiro(float mudador) {
 
         lerp = 0;
-        float objetivoLerp = anguloPonteiro + mudador;
+        float objetivoLerp = dinheiro + mudador;
 
         while(lerp < duracaoLerp) {
 
             lerp += Time.deltaTime;
-            anguloPonteiro = Mathf.Lerp(anguloPonteiro, objetivoLerp, lerp / duracaoLerp);
+            dinheiro = Mathf.Lerp(dinheiro, objetivoLerp, lerp / duracaoLerp);
             yield return null;
 
         }
 
-        anguloPonteiro = objetivoLerp;
+        dinheiro = objetivoLerp;
 
     }
 
-    public void lerparReputacao(float mudador) {
+    public void lerparDinheiro(float mudador) {
 
-        StartCoroutine(lerpReputacao(mudador));
+        StartCoroutine(lerpDinheiro(mudador));
 
     }
 
--- My project/Assets/Scripts/Teste2/Perso2.cs
+++ My project/Assets/Scripts/Teste2/Perso2.cs
@@ -13,6 +13,9 @@ public class Perso2 : MonoBehaviour
     [SerializeField] Text dinheiro;
     [SerializeField] GameObject ponteiro;
     [SerializeField] GameControl2 gameControl2;
+    
+    [SerializeField] float quantidadeRotacao;
+    [SerializeField] float quantidadeDinheiro;
      
     // Start is called before the first frame update
     void Start()
@@ -30,16 +33,39 @@ public class Perso2 : MonoBehaviour
 
         if(dinheiro != null) {
 
-            gameControl2.dinheiro += 10;
+            gameControl2.lerparDinheiro(quantidadeDinheiro);
 
         }
 
         if(ponteiro != null) {
 
-            gameControl2.lerparReputacao(-10);
+            gameControl2.comecarRodar(quantidadeRotacao);
 
         }
-        
+
+        gameControl2.controleAleatoriedade = gameControl2.personagemIndex;
+        while(gameControl2.controleAleatoriedade == gameControl2.personagemIndex) {
+            gameControl2.personagemIndex = Random.Range(0,gameControl2.personagens.Length);
+        }
+
+        gameControl2.nomeBalao.text = gameControl2.personagens[gameControl2.personagemIndex].name;
+        gameControl2.balaoTexto.text = gameControl2.personagens[gameControl2.personagemIndex].GetComponent<Perso2>().pedido;
+        gameControl2.botaoConfirmacaoTexto.text = gameControl2.personagens[gameControl2.personagemIndex].GetComponent<Perso2>().opcao1;
+        gameControl2.botaoNegacaoTexto.text = gameControl2.personagens[gameControl2.personagemIndex].GetComponent<Perso2>().opcao2;
+
+        gameControl2.botaoConfirmacao.onClick.RemoveAllListeners();
+        gameControl2.botaoNegacao.onClick.RemoveAllListeners();
+        gameControl2.botaoConfirmacao.onClick.AddListener(gameControl2.personagens[gameControl2.personagemIndex].GetComponent<Perso2>().Concordo);
+        gameControl2.botaoNegacao.onClick.AddListener(gameControl2.personagens[gameControl2.personagemIndex].GetComponent<Perso2>().Discordo);
+
+        gameControl2.quantidadePedidos ++;
+
+        if(gameControl2.quantidadePedidos >= 10) {
+
+            gameControl2.dia ++;
+            gameControl2.quantidadePedidos = 0;
+
+        }
 
     }
 
@@ -47,13 +73,38 @@ public class Perso2 : MonoBehaviour
 
         if(dinheiro != null) {
 
-            gameControl2.dinheiro -= 10;
+            gameControl2.lerparDinheiro(-quantidadeDinheiro);
 
         }
 
         if(ponteiro != null) {
 
-            gameControl2.lerparReputacao(10);
+            gameControl2.comecarRodar(-quantidadeRotacao);
+
+        }
+
+        gameControl2.controleAleatoriedade = gameControl2.personagemIndex;
+        while(gameControl2.controleAleatoriedade == gameControl2.personagemIndex) {
+            gameControl2.personagemIndex = Random.Range(0,gameControl2.personagens.Length);
+        }
+
+        gameControl2.personagemIndex = Random.Range(0,gameControl2.personagens.Length);
+        gameControl2.nomeBalao.text = gameControl2.personagens[gameControl2.personagemIndex].name;
+        gameControl2.balaoTexto.text = gameControl2.personagens[gameControl2.personagemIndex].GetComponent<Perso2>().pedido;
+        gameControl2.botaoConfirmacaoTexto.text = gameControl2.personagens[gameControl2.personagemIndex].GetComponent<Perso2>().opcao1;
+        gameControl2.botaoNegacaoTexto.text = gameControl2.personagens[gameControl2.personagemIndex].GetComponent<Perso2>().opcao2;
+
+        gameControl2.botaoConfirmacao.onClick.RemoveAllListeners();
+        gameControl2.botaoNegacao.onClick.RemoveAllListeners();
+        gameControl2.botaoConfirmacao.onClick.AddListener(gameControl2.personagens[gameControl2.personagemIndex].GetComponent<Perso2>().Concordo);
+        gameControl2.botaoNegacao.onClick.AddListener(gameControl2.personagens[gameControl2.personagemIndex].GetComponent<Perso2>().Discordo);
+
+        gameControl2.quantidadePedidos ++;
+
+        if(gameControl2.quantidadePedidos >= 10) {
+
+            gameControl2.dia ++;
+            gameControl2.quantidadePedidos = 0;
 
         }
 
